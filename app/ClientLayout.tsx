79ea1f031e755e8dc3acc0c7cc6d04d449881a03
samiadham12 <102438847+samiadham12@users.noreply.github.com>
2025-06-10 03:46:47 +0300

    Update ClientLayout.tsx
    .

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -76,6 +76,7 @@ export default function ClientLayout({
   }
   if (
     pathname == "/login" &&
+    pathname?.startsWith("/oauth")&&
     pathname == "/register" &&
     pathname == "/onboarding"
   ) {
