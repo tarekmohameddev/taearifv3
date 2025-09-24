30bae619acd45d5651343a54cd78b60e029ac6b0
samiadham12 <102438847+samiadham12@users.noreply.github.com>
2025-09-24 17:19:22 +0300

    Add Clarity tracking script to layout and app components for enhanced analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,20 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="ar" dir="rtl" className="light" suppressHydrationWarning>
+      <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ppln6ugd3t");
+            `,
+          }}
+        />
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,18 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ppln6ugd3t");
+            `,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
